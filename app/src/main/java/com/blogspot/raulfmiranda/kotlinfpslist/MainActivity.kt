395441b46ea28fb8ef339e2257f51c50ad905a1b
Raul Figueira <raulfmiranda@gmail.com>
2019-01-09 22:03:10 -0300

    Criação básica do layout

---
+++ app/src/main/java/com/blogspot/raulfmiranda/kotlinfpslist/MainActivity.kt
@@ -0,0 +1,14 @@
+package com.blogspot.raulfmiranda.kotlinfpslist
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    //TODO: https://youtu.be/tiXP__iYtq4?t=1418
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
