444fc83a4000aa6e254abeb06f54a328c2581091
Raul Figueira <raulfmiranda@gmail.com>
2019-01-09 22:05:02 -0300

    links com imagens

--- app/src/main/java/com/blogspot/raulfmiranda/kotlinfpslist/MainActivity.kt
+++ app/src/main/java/com/blogspot/raulfmiranda/kotlinfpslist/MainActivity.kt
@@ -7,6 +7,14 @@ class MainActivity : AppCompatActivity() {
 
     //TODO: https://youtu.be/tiXP__iYtq4?t=1418
 
+    /**
+      "https://images.pexels.com/photos/220421/pexels-photo-220421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      "https://images.pexels.com/photos/207891/pexels-photo-207891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      "https://images.pexels.com/photos/164854/pexels-photo-164854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      "https://images.pexels.com/photos/34533/owl-glitter-stuffed-animal-cute.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+      "https://images.pexels.com/photos/60023/baboons-monkey-mammal-freeze-60023.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260"
+    * */
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
