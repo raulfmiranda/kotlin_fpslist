d42bf262d089e05002cd48352dc6f2e5c1353681
Raul Figueira <raulfmiranda@gmail.com>
2019-01-18 19:39:16 -0300

    Vinculado o recyclerview a 5 itens estáticos

--- app/src/main/java/com/blogspot/raulfmiranda/kotlinfpslist/MainActivity.kt
+++ app/src/main/java/com/blogspot/raulfmiranda/kotlinfpslist/MainActivity.kt
@@ -2,6 +2,9 @@ package com.blogspot.raulfmiranda.kotlinfpslist
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.widget.LinearLayoutManager
+import android.widget.LinearLayout
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,5 +21,17 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val items = mutableListOf<Item>()
+        items.addAll(listOf(
+            Item("https://images.pexels.com/photos/220421/pexels-photo-220421.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Imagem 1"),
+            Item("https://images.pexels.com/photos/207891/pexels-photo-207891.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Imagem 2"),
+            Item("https://images.pexels.com/photos/164854/pexels-photo-164854.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Imagem 3"),
+            Item("https://images.pexels.com/photos/34533/owl-glitter-stuffed-animal-cute.jpg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Imagem 4"),
+            Item("https://images.pexels.com/photos/60023/baboons-monkey-mammal-freeze-60023.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260", "Imagem 5")
+        ))
+
+        recViewItems.layoutManager = LinearLayoutManager(this)
+        recViewItems.adapter = ItemAdapter(items)
     }
 }
